e112223d18f8b5a563a1b358cd38e045738e4547
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-04 11:58:10 +0530

    fix: fix some minor bugs

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight, ArrowRightToLine, Github } from "lucide-react";
 import Link from "next/link";
 
 export function Projects() {
@@ -31,14 +32,14 @@ export function Projects() {
 
       <div className="flex flex-col gap-10">
         {projects.map((project, index) => (
-          <div key={index} className="group">
+          <div key={index} className="">
             <div className="flex flex-col gap-1">
               <h3 className="text-lg font-medium">{project.title}</h3>
               <p className="text-xs text-muted-foreground">{project.date}</p>
-              <p className="text-xs flex gap-2">
+              <p className="flex gap-2">
                 {project.tags.map((tag, index) => {
                   return (
-                    <span key={index} className="bg-black/50 border border-white/25 px-2 py-1 rounded-4xl">{tag}</span>
+                    <span key={index} className="text-[0.6rem] text-accent bg-black/50 border border-accent/25 px-2 py-1 rounded-4xl">{tag}</span>
                   )
                 })}
               </p>
@@ -47,12 +48,20 @@ export function Projects() {
                   {project.content}
                 </div>
               )}
-              <Link
-                href={project.link}
-                className="text-sm transition-all opacity-75 hover:opacity-100"
-              >
-                Link to github
-              </Link>
+
+              <div className="flex gap-4 items-center">
+                <Link
+                  href={project.link}
+                  className="flex items-center gap-1 text-xs transition-all opacity-75 hover:opacity-100"
+                >
+                  <Github className="size-3" />Github
+                </Link>
+
+                <Link className="text-xs flex items-center gap-1 group" href={`/projects/${index}`}>
+                  Know more
+                  <ArrowRight className="size-3 pt-0 group-hover:translate-x-2 transition-all" />
+                </Link>
+              </div>
             </div>
           </div>
         ))}
