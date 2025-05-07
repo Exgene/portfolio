82a7032a4cf6fdf14e66978df5f2bd52c3294bf8
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-07 22:21:06 +0530

    fix: Some footer bug

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link"
 import { socials } from "./socials"
+
 export function Footer() {
-
   return (
-    <footer className="w-full pb-16 px-4 flex justify-between items-center">
-      <p className="text-sm opacity-75">© {new Date().getFullYear()} Kausthubh</p>
+    <footer className="w-full py-8 px-4 mt-auto">
+      <div className="max-w-4xl mx-auto flex justify-between items-center">
+        <p className="text-sm opacity-75">© {new Date().getFullYear()} Kausthubh</p>
 
-      <ul className="flex gap-4 flex-wrap">
-        {socials.map((link) => (
-          <Link
-            href={link.href}
-            className="text-sm transition-all opacity-75 hover:opacity-100"
-            key={link.name}
-          >
-            <link.icon className="size-4" />
-          </Link>
-        ))}
-      </ul>
+        <ul className="flex gap-4 flex-wrap">
+          {socials.map((link) => (
+            <Link
+              href={link.href}
+              className="text-sm transition-all opacity-75 hover:opacity-100"
+              key={link.name}
+            >
+              <link.icon className="size-4" />
+            </Link>
+          ))}
+        </ul>
+      </div>
     </footer>
   )
 }
