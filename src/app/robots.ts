c8ab6cb38ba7e2efb66739444721e46514a280d9
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-09 01:28:33 +0530

    chore: update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: [
-            { userAgent: "*", allow: "/" }
+            { userAgent: "*", allow: ["/", "/projects", "/blog"] }
         ],
         sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
     }
