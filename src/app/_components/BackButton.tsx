d9b8da201e4224f2c829f9b4b6e28899dc56ac1c
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-08 23:55:41 +0530

    fix: is it fixed?

--- src/app/_components/BackButton.tsx
+++ src/app/_components/BackButton.tsx
@@ -2,19 +2,14 @@
 
 import { ArrowLeft } from "lucide-react"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
 
 export default function BackButton({ id }: { id: string }) {
-    const router = useRouter()
     return (
-        <button
+        <Link
+            href={`/#${id}`}
             className="flex items-center gap-2 text-sm transition-all opacity-75 hover:opacity-100 w-fit"
-            onClick={(e) => {
-                e.preventDefault()
-                router.push(`/#${id}`)
-            }}
         >
             <ArrowLeft className="size-4" /> Back
-        </button>
+        </Link>
     )
 }
