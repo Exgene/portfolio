54e433bd38591b7b41e9460e6ddee8febfda04a0
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-08 23:43:46 +0530

    fix: edge runtime server rendered bug fix

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,5 +1,3 @@
-export const runtime = "edge"
-
 import { projects } from "@/app/_components/projects"
 import { ArrowRight, Github, ExternalLink, ArrowLeft } from "lucide-react"
 import Link from "next/link"
