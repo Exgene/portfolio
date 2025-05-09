ca13228752942d260aa16842b7ca19ecc4375e10
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-10 01:41:08 +0530

    LINKEDIN CHANGE

--- src/app/_components/socials.ts
+++ src/app/_components/socials.ts
@@ -1,7 +1,7 @@
 import { Github, Linkedin, Twitter, FileText, Mail } from "lucide-react";
 export const socials = [
   { name: "Twitter", href: "https://twitter.com/kaxsthxbh", icon: Twitter },
-  { name: "LinkedIn", href: "https://linkedin.com/in/kausthubh0", icon: Linkedin },
+  { name: "LinkedIn", href: "https://linkedin.com/in/kausthubhjrao", icon: Linkedin },
   { name: "GitHub", href: "https://github.com/exgene", icon: Github },
   { name: "Resume", href: "/resume.pdf", icon: FileText },
   { name: "Email", href: "mailto:kausthubhjrao@gmail.com", icon: Mail }
