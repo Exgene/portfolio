a78ce7617a219ec35354de5f3be26e7abf8e8299
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-10 00:46:19 +0530

    fix: mobile carousel

--- src/app/_components/ImageCarousel.tsx
+++ src/app/_components/ImageCarousel.tsx
@@ -15,19 +15,21 @@ export function ImageCarousel({ images, title }: { images: string[], title: stri
         {images.map((image, index) => (
           <CarouselItem key={index}>
             <div className="p-1 w-full h-full">
-              <Image 
-                src={image} 
-                alt={`${title} screenshot ${index + 1}`} 
-                width={800} 
-                height={450} 
-                className="object-contain w-full h-full" 
+              <Image
+                src={image}
+                alt={`${title} screenshot ${index + 1}`}
+                width={800}
+                height={450}
+                className="object-contain w-full h-full"
               />
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      <p className="sm:hidden block">You can swipe to move between images</p>
+
+      <CarouselPrevious className="sm:block hidden" />
+      <CarouselNext className="sm:block hidden" />
     </Carousel>
   )
 }
