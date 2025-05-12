cafd64d9a3be082da79d6259d8d70eb899bfd251
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-12 23:51:13 +0530

    chore: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,27 @@
 import { MetadataRoute } from "next";
 import { projects } from "./_components/projects";
+import { blogs } from "./_components/blogs";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const project_urls = projects.map((project) => ({
         url: `${process.env.NEXT_PUBLIC_URL}/projects/${project.id}`,
         lastModified: new Date().toISOString(),
+        priority: 0.8,
     }));
+
+    const blog_urls = blogs.map((post) => ({
+        url: `${process.env.NEXT_PUBLIC_URL}/blog/${post.id}`,
+        lastModified: new Date().toISOString(),
+        changeFrequency: "weekly",
+        priority: 1.0,
+    }));
+
     return [
         {
             url: process.env.NEXT_PUBLIC_URL!,
             lastModified: new Date().toISOString(),
             changeFrequency: "weekly",
+            priority: 0.9,
         },
         // {
         //     url: `${process.env.NEXT_PUBLIC_URL}/projects`,
@@ -24,5 +35,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             priority: 1,
         },
         ...project_urls,
+        ...blog_urls,
     ]
 }
