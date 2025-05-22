19526b96c885d07ba550ce29e33beea611c6deac
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-22 22:48:53 +0530

    chore: update resume name

--- src/app/_components/socials.ts
+++ src/app/_components/socials.ts
@@ -3,7 +3,7 @@ export const socials = [
   { name: "Twitter", href: "https://twitter.com/kaxsthxbh", icon: Twitter },
   { name: "LinkedIn", href: "https://linkedin.com/in/kausthubhjrao", icon: Linkedin },
   { name: "GitHub", href: "https://github.com/exgene", icon: Github },
-  { name: "Resume", href: "/resume-updated.pdf", icon: FileText },
+  { name: "Resume", href: "/resume.pdf", icon: FileText },
   { name: "Email", href: "mailto:kausthubhjrao@gmail.com", icon: Mail }
 ];
 
